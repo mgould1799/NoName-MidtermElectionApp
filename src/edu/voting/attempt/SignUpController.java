6fafaa24c10d00a511980f5a4f93d9131b9d4334
ehelm23 <42773318+ehelm23@users.noreply.github.com>
2018-10-23 09:27:34 -0400

    Update

--- src/edu/voting/attempt/SignUpController.java
+++ src/edu/voting/attempt/SignUpController.java
@@ -11,6 +11,21 @@ public class SignUpController {
 
     @FXML
     private javafx.scene.control.Button register;
+    @FXML
+    private javafx.scene.control.Button cancel;
+
+    public void pressCancel(ActionEvent event) throws Exception {
+        try{
+            cancel.getScene().getWindow().hide();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("loginPage.fxml"));
+            Parent root2 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root2));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void pressRegister(ActionEvent event) throws Exception {
         try{
